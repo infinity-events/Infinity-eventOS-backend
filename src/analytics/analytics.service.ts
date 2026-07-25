e8a942ecca68d4d42929480cc394b6dd40c18363
Andrea <andrea@example.com>
2026-07-25 19:57:49 +0200

    Update settings

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -1,144 +1,100 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-
+import {Injectable} from '@nestjs/common';
+import {PrismaService} from '../prisma/prisma.service';
 
 @Injectable()
 export class AnalyticsService{
-
 
 constructor(
 private prisma:PrismaService
 ){}
 
-
-
 async getAnalytics(festivalId:string){
 
-
 const tickets=await this.prisma.ticket.findMany({
-
 where:{
 festivalId
 }
-
 });
-
-
 
 const wristbands=await this.prisma.wristband.findMany({
-
 where:{
 festivalId
 }
-
 });
 
-
-
-const transactions=await this.prisma.transaction.findMany({
+const wallets=await this.prisma.wallet.findMany({
 
 where:{
-wallet:{
 user:{
-tickets:{
+wristbands:{
 some:{
 festivalId
 }
 }
 }
-}
+},
+
+include:{
+transactions:true
 }
 
 });
 
+let topups=0;
+let spent=0;
 
+wallets.forEach(wallet=>{
+wallet.transactions.forEach(transaction=>{
 
-const ticketCategories={};
-
-
-tickets.forEach(ticket=>{
-
-
-if(!ticketCategories[ticket.type]){
-
-ticketCategories[ticket.type]=0;
-
+if(transaction.type==="TOPUP"){
+topups+=transaction.amount;
 }
 
+if(transaction.type==="PURCHASE"){
+spent+=transaction.amount;
+}
+});
+});
+
+const ticketCategories={};
+tickets.forEach(ticket=>{
+    if(!ticketCategories[ticket.type]){
+    ticketCategories[ticket.type]=0;
+}
 
 ticketCategories[ticket.type]++;
 
-
 });
-
-
-
-const revenue=tickets.reduce(
-(sum,ticket)=>sum+ticket.price,
-0
-);
-
-
-
-const walletTopup=transactions
-.filter(t=>t.type==="TOPUP")
-.reduce(
-(sum,t)=>sum+t.amount,
-0
-);
-
-
-
-const walletSpent=transactions
-.filter(t=>t.type==="PURCHASE")
-.reduce(
-(sum,t)=>sum+t.amount,
-0
-);
-
-
 
 return {
 
-
 tickets:{
-
 sold:tickets.length,
-
-revenue,
-
+revenue:tickets.reduce(
+(sum,ticket)=>sum+ticket.price,
+0
+),
 categories:ticketCategories
-
 },
 
-
-
 wristbands:{
-
 total:wristbands.length,
-
 activated:wristbands.filter(
 w=>w.activated
 ).length
 
 },
 
-
-
 wallet:{
 
-topups:walletTopup,
+topups,
 
-spent:walletSpent
+spent
 
+},
+event:{
+users:wallets.length
 }
-
-
-
 };
-
-
 }
-
-
 }
