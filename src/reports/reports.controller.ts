4c1ae6c915b1dfe230c7fddbccfc58c6e12a7468
Andrea <andrea@example.com>
2026-07-28 21:09:05 +0200

    Update analytics

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,16 +1,40 @@
 import { Controller, Post } from '@nestjs/common';
 import { ReportsService } from './reports.service';
+import { ReportsMailService } from './reports.mail';
+
 
 @Controller('reports')
 export class ReportsController {
 
     constructor(
-        private readonly reportsService: ReportsService
+        private readonly reportsService: ReportsService,
+        private readonly reportsMailService: ReportsMailService
     ) {}
 
+
     @Post('generate')
-    async generateReport(){
+    generate(){
         return this.reportsService.generateWeeklyReports();
+    }
+
+
+    @Post('email-test')
+    async emailTest(){
+
+        const report =
+            await this.reportsService.generateWeeklyReports();
+
+
+        await this.reportsMailService.sendReport(
+            'LA_TUA_EMAIL',
+            report.pdf
+        );
+
+
+        return {
+            message: 'Email inviata'
+        };
+
     }
 
 }
