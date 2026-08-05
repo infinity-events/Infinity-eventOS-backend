b14ddf6855bcf1d377469c89b1b3c309a583c2c4
Andrea <andrea@example.com>
2026-08-05 14:39:08 +0200

    update user managing

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FirebaseAdminService } from './firebase-admin.service';
 import { FirebaseAuthGuard } from './firebase-auth.guard';
+import { PrismaModule } from '../prisma/prisma.module';
 
 
 @Module({
+  imports:[
+    PrismaModule
+  ],
   providers:[
     FirebaseAdminService,
     FirebaseAuthGuard
