0ab924dfac3e5e54bd4290dda5cf67600c4be8d6
Andrea <andrea@example.com>
2026-08-17 11:45:15 +0200

    add stripe payment method

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -6,7 +6,7 @@ import { PrismaService } from '../prisma/prisma.service';
 export class StripeService {
   private stripe?: Stripe;
   private readonly apiUrl = process.env.API_PUBLIC_URL || 'https://infinity-eventos-api.onrender.com';
-  private readonly vividFestUrl = process.env.VIVIDFEST_URL || 'https://vividfest.vercel.app/authenticated.html#tickets';
+  private readonly vividFestUrl = process.env.VIVIDFEST_URL || 'https://vividfest.vercel.app/payment-success.html';
 
   constructor(private readonly prisma: PrismaService) {}
 
