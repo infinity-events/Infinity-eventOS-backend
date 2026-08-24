7d89c4344d1c277e70be5a93360e9bdbecad5848
Andrea <andrea@example.com>
2026-08-24 11:51:02 +0200

    fix POS prisma dependency

--- src/pos/pos.module.ts
+++ src/pos/pos.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 
 import { PosController } from './pos.controller';
 import { PosService } from './pos.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  controllers: [PosController],
-  providers: [PosService],
-  exports: [PosService],
+  imports: [
+    PrismaModule,
+  ],
+  controllers: [
+    PosController,
+  ],
+  providers: [
+    PosService,
+  ],
+  exports: [
+    PosService,
+  ],
 })
 export class PosModule {}
